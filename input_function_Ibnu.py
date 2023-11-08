f5caee4409ee8c4759853692b188fff5ec6f54ef
bintangputranto <150228032+bintangputranto@users.noreply.github.com>
2023-11-08 14:28:11 +0700

    Add files via upload

---
+++ input_function_Ibnu.py
@@ -0,0 +1,15 @@
+def penyapa_Ibnu(garis_strip, nama):
+     
+     print(garis_strip)
+     print(garis_strip)
+     print()
+     
+     print(f"Halo {nama}! Apa kabarmu?")
+     
+     print()
+     print(garis_strip)
+     print(garis_strip)
+
+penyapa_Ibnu("❤❤❤❤❤❤❤", "Isabel")
+penyapa_Ibnu("‼‼‼‼‼‼‼‼", "Rony")
+penyapa_Ibnu("➕➕➕➕➕➕➕", "Subagyo")
---
+++ input_seafoods.py
@@ -0,0 +1,19 @@
+garis_strip = "---------------"
+data_user = input("masukkan data kamu kesini: ")
+nama_user = input("nama kamu: ")
+
+print(garis_strip)
+print(garis_strip)
+
+print( )
+
+print(f"{nama_user} data kamu adalah: ")
+
+print( )
+for i in data_user:
+    print(i)
+
+print( )
+
+print(garis_strip)
+print(garis_strip)
