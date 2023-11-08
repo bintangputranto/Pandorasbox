5e85965ebf41f1aa6085c4618326f61f32853804
bintangputranto <150228032+bintangputranto@users.noreply.github.com>
2023-11-08 14:32:01 +0700

    Update input_seafoods.py
    Masukkan kode ke dalam function.

--- input_seafoods.py
+++ input_seafoods.py
@@ -1,19 +1,23 @@
-garis_strip = "---------------"
-data_user = input("masukkan data kamu kesini: ")
-nama_user = input("nama kamu: ")
+def aplikasi_run():
+    
+    garis_strip = "---------------"
+    data_user = input("masukkan data kamu kesini: ")
+    nama_user = input("nama kamu: ")
+    
+    print(garis_strip)
+    print(garis_strip)
+    
+    print( )
+    
+    print(f"{nama_user} data kamu adalah: ")
+    
+    print( )
+    for i in data_user:
+        print(i)
+    
+    print( )
+    
+    print(garis_strip)
+    print(garis_strip)
 
-print(garis_strip)
-print(garis_strip)
-
-print( )
-
-print(f"{nama_user} data kamu adalah: ")
-
-print( )
-for i in data_user:
-    print(i)
-
-print( )
-
-print(garis_strip)
-print(garis_strip)
+aplikasi_run()
